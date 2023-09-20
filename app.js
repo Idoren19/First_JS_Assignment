e75ea3cb6b56b4631856fdffeebcf50011b4469a
Anietie Idorenyin <Anietieidoren@gmail.com>
2023-09-20 23:45:59 +0100

    my first js assignment

---
+++ app.js
@@ -0,0 +1,17 @@
+function kgToPounds(name, weightKg) {
+    
+    const weightPounds = weightKg * 2.20462;
+    
+    const resultString = `${name}, your weight is ${weightPounds.toFixed(2)} pounds.`;
+    
+    return resultString;
+  }
+  
+  const userName = prompt("Please enter your name:");
+  
+  const userWeightKg = parseFloat(prompt("Please enter your weight in kilograms:"));
+  
+  
+  const result = kgToPounds(userName, userWeightKg);
+  alert(result);
+  
